566768641aa9445b16eafd471099f9c6831c8828
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-11-27 21:24:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const key = 'feedback-form-state';
 
+// check if storage has data when the page loaded
+const storageData = localStorage.getItem(key);
+if (storageData) {
+  storageDataObj = JSON.parse(storageData);
+
+  form.email.value = storageDataObj.email;
+  form.message.value = storageDataObj.message;
+}
+
+// on input
+
+const storageDataUpdate = () => {
+  localStorage.setItem(
+    key,
+    JSON.stringify({
+      email: form.email.value,
+      message: form.message.value,
+    })
+  );
+};
+form.addEventListener('input', throttle(storageDataUpdate, 500));
+
+// // on submit click
+const onSubmit = ev => {
+  ev.preventDefault();
+  console.log(localStorage.getItem(key));
+  localStorage.removeItem(key);
+  form.reset();
+};
+form.addEventListener('submit', onSubmit);
